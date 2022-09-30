ee698c64bd8aa958684591d6ee06b00dbacfa6e9
DisuAde <idrisadekunle726@gmail.com>
2022-09-30 12:17:06 +0100

    to be flip

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 /* const mate  = new Date().toLocaleTimeString()
 the date is initiated as the page loads, putting it in afunction solves it being static
 */
+const dayes = document.querySelector('.days')
+const hrs = document.querySelector('.hours')
+const mines = document.querySelector('.mins')
+const secs = document.querySelector('.secs') 
+
 setInterval(()=> myTimer(),
 1000)
 
 function myTimer() {
    const d = new Date();
-   document.body.innerHTML = (d.toLocaleTimeString()) ;
+ console.log(d.toLocaleTimeString()) ;
    const distance = newd -d
    const days = Math.floor(distance/(1000*60*60*24))
    const hours =  Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
- console.log(days,hours,minutes,seconds);
+   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//  console.log(days,hours,minutes,seconds);
+
+dayes.innerHTML = days
+hrs.innerHTML = hours
+mines.innerHTML = minutes
+secs.innerHTML = seconds
+ seconds === 0?mines.classList.add('rets'): mines.classList.remove('rets')
+secs.classList.add('rets')
+
 
  }
  const newd  = new Date("Jan 1, 2023 00:00:00").getTime()
  
- console.log(newd);
