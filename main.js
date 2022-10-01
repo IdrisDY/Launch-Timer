f1253c2e4a00863271f73133a0fba3339432611d
DisuAde <idrisadekunle726@gmail.com>
2022-10-01 21:19:30 +0100

    gsap add

--- main.js
+++ main.js
@@ -18,14 +18,31 @@ function myTimer() {
    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 //  console.log(days,hours,minutes,seconds);
+function setTimes(){
+  dayes.innerHTML = days
+  hrs.innerHTML = hours
+  mines.innerHTML = minutes
+  // secs.innerHTML = seconds
+  animeFlip(secs, seconds)
+}
+setTimes()
+//  seconds === 0?mines.classList.add('rets'): mines.classList.remove('rets')
+// secs.classList.add('rets')
+function animeFlip(elem,val){
+  elem.querySelector('.top-back span').innerText = val
+  elem.querySelector('.bottom-back span').innerText = val
+gsap.to(elem.querySelector('.top'),0.7,{
+rotationX:'-90deg',
+transformPerspective:300,
+ease:CountQueuingStrategy.easeOut,
+onComplete:()=>{
+  elem.querySelector('.top').innerText = val
+  elem.querySelector('.bottom').innerText = val
 
-dayes.innerHTML = days
-hrs.innerHTML = hours
-mines.innerHTML = minutes
-secs.innerHTML = seconds
- seconds === 0?mines.classList.add('rets'): mines.classList.remove('rets')
-secs.classList.add('rets')
+}
+})
 
+}
 
  }
  const newd  = new Date("Jan 1, 2023 00:00:00").getTime()
