e1e42d5f8cc0ff8bf7c29723e933a4cd23216639
DisuAde <idrisadekunle726@gmail.com>
2022-10-23 07:20:20 +0100

    commented , new readme

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-/* const mate  = new Date().toLocaleTimeString()
-the date is initiated as the page loads, putting it in a function solves it being static
-*/
 const dayes = document.querySelector('.days')
 const hrs = document.querySelector('.hours')
 const mines = document.querySelector('.mins')
 const secs = document.querySelector('.secs') 
+const newd  = new Date("Jan 1, 2023 00:00:00").getTime()
 
 setInterval(()=> myTimer(),
 1000)
 
 function myTimer() {
    const d = new Date();
- console.log(d.toLocaleTimeString()) ;
    const distance = newd -d
    const days = Math.floor(distance/(1000*60*60*24))
    const hours =  Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-//  console.log(days,hours,minutes,seconds);
+
+/**
+ * The function setTimes() calls the function animeFlip() four times, passing in the variables secs,
+ * seconds, mines, minutes, dayes, days, hrs, and hours.
+ * Setting the time and flip animation.
+ */
 function setTimes(){
    animeFlip(secs, seconds)
   animeFlip(mines, minutes)
@@ -25,16 +27,21 @@ function setTimes(){
   animeFlip(hrs, hours)
 }
 setTimes()
-//  seconds === 0?mines.classList.add('rets'): mines.classList.remove('rets')
-// secs.classList.add('rets')
+
 function animeFlip(elem,val){
   const backVal = elem.querySelector('.bottom').innerText
+
+/* Adds 0 to number less than 10. */
   const currentVal = val < 10 ? `0${val}`: '' + val
-console.log(currentVal);
+
   if (backVal === currentVal)return;
 
   elem.querySelector('.top-back span').innerText =currentVal
   elem.querySelector('.bottom-back span').innerText =currentVal
+
+/* Animation function from gsap library to animate flip
+* effects in an object format
+*/
 gsap.to(elem.querySelector('.top'),0.7,{
 rotationX:'-180deg',
 transformPerspective:300,
@@ -46,16 +53,15 @@ onComplete:()=>{
   )
 }
 })
+
 gsap.to(elem.querySelector('.top-back'),0.7,{
   rotationX:0,
   transformPerspective:300,
   ease:Quart.easeOut,
   clearProps:'all'
-
   })
   
 }
 
  }
- const newd  = new Date("Jan 1, 2023 00:00:00").getTime()
  
